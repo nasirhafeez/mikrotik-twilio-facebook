ec368ae54de86d2627fde17b3e939fe38caca532
root <root@instance-1.us-central1-a.c.crypto-truck-248421.internal>
2020-11-12 07:42:57 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+
+include 'parameters.php';
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+$fb = new Facebook\Facebook([
+  'app_id'                => $_SERVER['APP_ID'],
+  'app_secret'            => $_SERVER['APP_SECRET'],
+  'default_graph_version' => $_SERVER['DEFAULT_GRAPH_VERSION'],
+]);
+
+$helper      = $fb->getRedirectLoginHelper();
+$scope       = array("email");
+$loginUrl    = $helper->getLoginUrl($callBackUrl,$scope);
+
+$_SESSION["mac"]=$_POST['mac'];
+$_SESSION["ip"]=$_POST['ip'];
+$_SESSION["linkorig"]=$_POST['link-orig'];
+$_SESSION["linkloginonly"]=$_POST['link-login-only'];
+
+$_SESSION["user_type"] = "new";
+$_SESSION["method"] = "Form";
+
+/*
+Checking DB to see if user exists or not.
+*/
+
+$host_ip = $_SERVER['HOST_IP'];
+$db_user = $_SERVER['DB_USER'];
+$db_pass = $_SERVER['DB_PASS'];
+$db_name = $_SERVER['DB_NAME'];
+
+$con = mysqli_connect($host_ip, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to SQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[mac]'");
+
+if ($result->num_rows >= 1) {
+  $row = mysqli_fetch_array($result);
+
+  $_SESSION["fname"] = $row[1];
+  $_SESSION["lname"] = $row[2];
+  $_SESSION["email"] = $row[3];
+  $_SESSION["method"] = $row[5];
+
+  mysqli_close($con);
+
+  $_SESSION["user_type"] = "repeat";
+  header("Location: welcome.php");
+} else {
+  mysqli_close($con);
+}
+
+?>
+
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Astiisb WiFi</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <link rel="stylesheet" href="bulma.min.css" />
+  <link rel="stylesheet" href="font-awesome\css\font-awesome.min.css" />
+  <script defer src="vendor\fortawesome\font-awesome\js\all.js"></script>
+  <link rel="icon" type="image/png" href="favicomatic\favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="favicomatic\favicon-16x16.png" sizes="16x16" />
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="bg">
+
+    <figure id="logo">
+      <img src="logo.png">
+    </figure>
+
+    <div id="login" class="content is-size-4 has-text-weight-bold">Login for Free Wi-Fi</div>
+
+    <form id="login_success" class="form_login" method="post" action="connect.php">
+      <br>
+      
+      <div class="field">
+        <div class="control has-icons-left">
+          <input class="input" type="text" id="form_font" name="fname" placeholder="First Name" required>
+          <span class="icon is-small is-left">
+            <i class="fas fa-user"></i>
+          </span>
+        </div>
+      </div>
+      
+      <div class="field">
+        <div class="control has-icons-left">
+          <input class="input" type="text" id="form_font" name="lname" placeholder="Last Name" required>
+          <span class="icon is-small is-left">
+            <i class="fas fa-user"></i>
+          </span>
+        </div>
+      </div>
+      
+      <div class="field">
+        <div class="control has-icons-left">
+          <input class="input" type="email" id="form_font" name="email" placeholder="Email" required>
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+        </div>
+      </div>
+      
+      <div id="access_wifi" class="control">
+        <button id="button_font" class="button is-danger">Continue</button>
+      </div>
+                        
+    </form>
+
+    <div id="logintext" class="content is-size-6">Or login using:</div>
+    
+    <div id="social">
+      <a href="<?php echo htmlspecialchars($loginUrl);?>" class="facebookBtn smGlobalBtn"></a>
+    </div>
+    <br>
+
+    <div id="powered" class="content is-size-6">Powered by Astiisb</div>
+    <div id="copyright" class="content is-size-6">(C) Copyright 2020</div>
+
+  </div>
+
+</body>
+</html>
